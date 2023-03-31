dcb6e11b3380c4a17a8ee07d73de70ba384f9a96
James Leonardi <james.leonardi@stonybrook.edu>
2023-03-31 15:24:09 -0400

    controls

--- src/hw3/HW3Controls.ts
+++ src/hw3/HW3Controls.ts
@@ -1,9 +1,12 @@
 /**
- * A set of controls for the HW4 keybinding
+ * A set of controls for keybinding
  */
 export const HW3Controls = {
     MOVE_LEFT: "MOVE_LEFT",
     MOVE_RIGHT: "MOVE_RIGHT",
     JUMP: "JUMP",
-    ATTACK: "ATTACK"
+    SWITCH: "SWITCH",
+    GRAPPLE: "GRAPPLE",
+    ATTACK: "ATTACK",
+    PEEK: "PEEK"
 } as const;
