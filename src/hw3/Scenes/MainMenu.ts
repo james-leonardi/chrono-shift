a8d477510343b4ca35a74f014dbf1128a5b1c087
Kevin Tao <kevin.tao@stonybrook.edu>
2023-03-31 15:12:46 -0400

    ui switching and backgrounds

--- src/hw3/Scenes/MainMenu.ts
+++ src/hw3/Scenes/MainMenu.ts
@@ -12,6 +12,7 @@ import Level1 from "./HW3Level1";
 // Layers for the main menu scene
 export const MenuLayers = {
     MAIN: "MAIN",
+    LEVELSELECT: "LEVELSELECT",
     CONTROLS: "CONTROLS",
     ABOUT: "ABOUT"
 } as const;
@@ -30,6 +31,7 @@ export default class MainMenu extends Scene {
     public static readonly BACKGROUND_PATH = "hw4_assets/Background.png";
     private mainMenu: Layer;
     private controls: Layer;
+    private levelSelect: Layer;
     private about: Layer;
     private logo: Sprite;
     private chrono: Sprite;
@@ -50,6 +52,9 @@ export default class MainMenu extends Scene {
         this.controls = this.addUILayer(MenuLayers.CONTROLS);
         this.controls.setHidden(true);
 
+        this.levelSelect = this.addUILayer(MenuLayers.LEVELSELECT);
+        this.levelSelect.setHidden(true);
+
         this.about = this.addUILayer(MenuLayers.ABOUT);
         this.about.setHidden(true);
 
@@ -59,14 +64,13 @@ export default class MainMenu extends Scene {
         this.viewport.setZoomLevel(1);
 
         // Background
-        let background = this.add.sprite(MainMenu.BACKGROUND_KEY, MenuLayers.MAIN);
-        background.position.set(size.x, size.y);
-        background.scale.set(0.7, 0.7);
+        for (const scene of [MenuLayers.MAIN, MenuLayers.CONTROLS, MenuLayers.ABOUT, MenuLayers.LEVELSELECT]) {
+            const background = this.add.sprite(MainMenu.BACKGROUND_KEY, scene);
+            background.position.set(size.x, size.y);
+            background.scale.set(0.7, 0.7);
+        }
 
         // Logo stuff
-        /* this.logo = this.add.sprite(MainMenu.LOGO_KEY, MenuLayers.MAIN);
-        this.logo.position.set(size.x, size.y - 200);
-        this.logo.scale.set(0.72, 0.72); */
         this.chrono = this.add.sprite(MainMenu.CHRONO_KEY, MenuLayers.MAIN);
         this.chrono.position.set(size.x - 355, size.y - 220);
         this.chrono.scale.set(0.65, 0.65);
@@ -74,54 +78,53 @@ export default class MainMenu extends Scene {
         this.switch.position.set(size.x + 355, size.y - 220);
         this.switch.scale.set(0.65, 0.65);
 
+        const defaultProperties = { backgroundColor: Color.WHITE, borderColor: Color.WHITE, textColor: Color.BLACK, borderRadius: 10, font: "MyFont" }
 
-        // Create a play button
-        let playBtn = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, {position: new Vec2(size.x, size.y+10), text: "PLAY"});
-        playBtn.backgroundColor = Color.WHITE;
-        playBtn.borderColor = Color.WHITE;
-        playBtn.textColor = Color.BLACK;
-        playBtn.borderRadius = 10;
-        playBtn.size = new Vec2(240, 80);
-        playBtn.fontSize = 42;
-        playBtn.setPadding(new Vec2(50, 15));
-        playBtn.font = "MyFont";
+        {   /* Main Menu */
+            // Create a play button
+            let playBtn = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, {position: new Vec2(size.x, size.y+10), text: "PLAY"});
+            Object.assign(playBtn, defaultProperties);
+            playBtn.size = new Vec2(240, 80);
+            playBtn.fontSize = 42;
+            playBtn.setPadding(new Vec2(50, 15));
 
-        // Create a LevelSelect button
-        let levelSelect = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x, size.y + 90), text: "LEVELS" });
-        levelSelect.backgroundColor = Color.WHITE;
-        levelSelect.borderColor = Color.WHITE;
-        levelSelect.textColor = Color.BLACK;
-        levelSelect.borderRadius = 10;
-        levelSelect.size = new Vec2(185, 55);
-        levelSelect.fontSize = 30;
-        levelSelect.setPadding(new Vec2(50, 15));
-        levelSelect.font = "MyFont";
+            // Create a LevelSelect button
+            let levelSelect = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x, size.y + 90), text: "LEVELS" });
+            Object.assign(levelSelect, defaultProperties);
+            levelSelect.size = new Vec2(185, 55);
+            levelSelect.fontSize = 30;
+            levelSelect.setPadding(new Vec2(50, 15));
 
-        // Create a controls button
-        let controls = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x-130, size.y + 160), text: "CONTROLS" });
-        controls.backgroundColor = Color.WHITE;
-        controls.borderColor = Color.WHITE;
-        controls.textColor = Color.BLACK;
-        controls.borderRadius = 10;
-        controls.size = new Vec2(205, 55);
-        controls.fontSize = 30;
-        controls.setPadding(new Vec2(50, 15));
-        controls.font = "MyFont";
+            // Create a controls button
+            let controls = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x - 130, size.y + 160), text: "CONTROLS" });
+            Object.assign(controls, defaultProperties);
+            controls.size = new Vec2(205, 55);
+            controls.fontSize = 30;
+            controls.setPadding(new Vec2(50, 15));
 
-        // Create an about button
-        let about = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x+130, size.y + 160), text: "ABOUT" });
-        about.backgroundColor = Color.WHITE;
-        about.borderColor = Color.WHITE;
-        about.textColor = Color.BLACK;
-        about.borderRadius = 10;
-        about.size = new Vec2(205, 55);
-        about.fontSize = 30;
-        about.setPadding(new Vec2(50, 15));
-        about.font = "MyFont";
+            // Create an about button
+            let about = <Button>this.add.uiElement(UIElementType.BUTTON, MenuLayers.MAIN, { position: new Vec2(size.x + 130, size.y + 160), text: "ABOUT" });
+            Object.assign(about, defaultProperties);
+            about.size = new Vec2(205, 55);
+            about.fontSize = 30;
+            about.setPadding(new Vec2(50, 15));
+            // When the play button is clicked, go to the next scene
 
-        // When the play button is clicked, go to the next scene
-        playBtn.onClick = () => {
-            this.sceneManager.changeToScene(Level1);
+            playBtn.onClick = () => {
+                this.sceneManager.changeToScene(Level1);
+            }
+            levelSelect.onClick = () => {
+                this.mainMenu.setHidden(true);
+                this.levelSelect.setHidden(false);
+            }
+            controls.onClick = () => {
+                this.mainMenu.setHidden(true);
+                this.controls.setHidden(false);
+            }
+            about.onClick = () => {
+                this.mainMenu.setHidden(true);
+                this.about.setHidden(false);
+            }
         }
 
         // Scene has started, so start playing music
