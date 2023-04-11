1d22fb6c77084379d3a9292cce8bbaa4dcf1b8a1
Kyle Yee <WatermaliaFTW@gmail.com>
2023-04-10 23:11:24 -0400

    update to latest for benchmark2

---
+++ public/benchmark2/src/hw3/Player/PlayerStates/Dead.ts
@@ -0,0 +1,30 @@
+import GameEvent from "../../../Wolfie2D/Events/GameEvent";
+import { PlayerTweens } from "../PlayerController";
+import { GameEventType } from "../../../Wolfie2D/Events/GameEventType";
+import PlayerState from "./PlayerState";
+import { HW3Events } from "../../HW3Events";
+
+/**
+ * The Dead state for the player's FSM AI. 
+ */
+export default class Dead extends PlayerState {
+
+    // Trigger the player's death animation when we enter the dead state
+    public onEnter(options: Record<string, any>): void {
+        this.owner.tweens.play(PlayerTweens.DEATH);
+        this.owner.freeze();
+        /* this.owner.animation.play("DYING", false, undefined);
+        this.owner.animation.queue("DEATH", false, HW3Events.PLAYER_DEAD); */
+        let deadgeAudio = this.owner.getScene().getDeadgeAudioKey();
+        //this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: deadgeAudio, loop: false, holdReference: false});
+    }
+
+    // Ignore all events from the rest of the game
+    public handleInput(event: GameEvent): void { }
+
+    // Empty update method - if the player is dead, don't update anything
+    public update(deltaT: number): void {}
+
+    public onExit(): Record<string, any> { return {}; }
+    
+}
---
+++ public/benchmark2/src/hw3/Scenes/HW3Level1.ts
@@ -0,0 +1,127 @@
+import AABB from "../../Wolfie2D/DataTypes/Shapes/AABB";
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import HW3Level from "./HW3Level";
+import RenderingManager from "../../Wolfie2D/Rendering/RenderingManager";
+import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import SceneManager from "../../Wolfie2D/Scene/SceneManager";
+import Viewport from "../../Wolfie2D/SceneGraph/Viewport";
+import HW4Level2 from "./HW3Level2";
+
+/**
+ * The first level for HW4 - should be the one with the grass and the clouds.
+ */
+export default class Level1 extends HW3Level {
+
+    public static readonly PLAYER_SPAWN = new Vec2(32, 608);
+    public static readonly PLAYER_SPRITE_KEY = "PLAYER_SPRITE_KEY";
+    public static readonly PLAYER_SPRITE_PATH = "hw4_assets/spritesheets/Tepster.json";
+
+    public static readonly TILEMAP_KEY = "LEVEL1";
+    public static readonly TILEMAP_PATH = "hw4_assets/tilemaps/testlevel.json";
+    public static readonly TILEMAP_SCALE = new Vec2(2, 2);
+    public static readonly DESTRUCTIBLE_LAYER_KEY = "Main";
+    public static readonly DEATH_LAYER_KEY = "Death";
+    public static readonly WALLS_LAYER_KEY = "Main";
+
+    public static readonly LEVEL_MUSIC_KEY = "LEVEL_MUSIC";
+    public static readonly LEVEL_MUSIC_PATH = "hw4_assets/music/bgm.mp3";
+
+    public static readonly JUMP_AUDIO_KEY = "PLAYER_JUMP";
+    public static readonly JUMP_AUDIO_PATH = "hw4_assets/sounds/jump.wav";
+    
+    public static readonly DAMAGED_AUDIO_KEY = "PLAYER_DAMAGED";
+    public static readonly DAMAGED_AUDIO_PATH = "hw4_assets/sounds/takedamage.mp3";
+
+    public static readonly DEADGE_AUDIO_KEY = "PLAYER_DEADGE";
+    public static readonly DEADGE_AUDIO_PATH = "hw4_assets/sounds/deadge.mp3";
+
+    public static readonly TILE_DESTROYED_KEY = "TILE_DESTROYED";
+    public static readonly TILE_DESTROYED_PATH = "hw4_assets/sounds/switch.wav";
+
+    public static readonly LEVEL_END = new AABB(new Vec2(928, 3632), new Vec2(24, 16));
+
+    public constructor(viewport: Viewport, sceneManager: SceneManager, renderingManager: RenderingManager, options: Record<string, any>) {
+        super(viewport, sceneManager, renderingManager, options);
+
+        // Set the keys for the different layers of the tilemap
+        this.tilemapKey = Level1.TILEMAP_KEY;
+        this.tilemapScale = Level1.TILEMAP_SCALE;
+        this.destructibleLayerKey = Level1.DESTRUCTIBLE_LAYER_KEY;
+        this.wallsLayerKey = Level1.WALLS_LAYER_KEY;
+        this.deathLayerKey = Level1.DEATH_LAYER_KEY;
+
+        // Set the key for the player's sprite
+        this.playerSpriteKey = Level1.PLAYER_SPRITE_KEY;
+        // Set the player's spawn
+        this.playerSpawn = Level1.PLAYER_SPAWN;
+
+        // Music and sound
+        this.levelMusicKey = Level1.LEVEL_MUSIC_KEY
+        this.jumpAudioKey = Level1.JUMP_AUDIO_KEY;
+        this.tileDestroyedAudioKey = Level1.TILE_DESTROYED_KEY;
+        this.damagedAudioKey = Level1.DAMAGED_AUDIO_KEY;
+        this.deadgeAudioKey = Level1.DEADGE_AUDIO_KEY;
+
+        // Level end size and position
+        this.levelEndPosition = new Vec2(54, 132).mult(this.tilemapScale);
+        this.levelEndHalfSize = new Vec2(32, 32).mult(this.tilemapScale);
+        this.pastPosition = new Vec2(688, 1584).mult(this.tilemapScale);
+    }
+
+    /**
+     * Load in our resources for level 1
+     */
+    public loadScene(): void {
+        // Load in the tilemap
+        this.load.tilemap(this.tilemapKey, Level1.TILEMAP_PATH);
+        // Load in the player's sprite
+        this.load.spritesheet(this.playerSpriteKey, Level1.PLAYER_SPRITE_PATH);
+        // Audio and music
+        this.load.audio(this.levelMusicKey, Level1.LEVEL_MUSIC_PATH);
+        this.load.audio(this.jumpAudioKey, Level1.JUMP_AUDIO_PATH);
+        this.load.audio(this.tileDestroyedAudioKey, Level1.TILE_DESTROYED_PATH);
+        this.load.audio(this.damagedAudioKey, Level1.DAMAGED_AUDIO_PATH);
+        this.load.audio(this.deadgeAudioKey, Level1.DEADGE_AUDIO_PATH);
+
+        //this.load.audio("GRAPPLE_0", "hw4_assets/sounds/grapple_0.mp3");
+        //this.load.audio("GRAPPLE_1", "hw4_assets/sounds/grapple_1.mp3");
+        //this.load.audio("GRAPPLE_2", "hw4_assets/sounds/grapple_2.mp3");
+        this.load.audio("ZIP_0", "hw4_assets/sounds/zip1.mp3");
+        this.load.audio("ZIP_1", "hw4_assets/sounds/zip2.mp3");
+        this.load.audio("PSHH", "hw4_assets/sounds/pshh.mp3");
+        this.load.audio("SWITCH_1", "hw4_assets/sounds/switch1.wav");
+        this.load.audio("SWITCH_2", "hw4_assets/sounds/switch2.wav");
+        //this.load.audio("WIN", "hw4_assets/sounds/imsosorry.mp3");
+    }
+
+    /**
+     * Unload resources for level 1
+     */
+    public unloadScene(): void {
+        // TODO decide which resources to keep/cull 
+        this.emitter.fireEvent(GameEventType.STOP_SOUND, {key: Level1.LEVEL_MUSIC_KEY});
+        this.load.keepAudio(this.jumpAudioKey);
+        this.load.keepAudio(this.tileDestroyedAudioKey);
+        this.load.keepAudio(this.damagedAudioKey);
+        this.load.keepAudio(this.deadgeAudioKey);
+        this.load.keepSpritesheet(this.playerSpriteKey);
+    }
+
+    public startScene(): void {
+        super.startScene();
+        // Set the next level to be Level2
+        this.nextLevel = HW4Level2;
+    }
+
+    /**
+     * I had to override this method to adjust the viewport for the first level. I screwed up 
+     * when I was making the tilemap for the first level is what it boils down to.
+     * 
+     * - Peter
+     */
+    protected initializeViewport(): void {
+        super.initializeViewport();
+        this.viewport.setBounds(0, 0, 2752, 4096);
+    }
+
+}
